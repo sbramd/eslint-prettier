693678ec2f58720f47fd39c3004324a1336d3b39
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Pro.local>
2020-03-05 16:00:27 +0100

    Still testing

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
-				<a
+			<a
 					className="App-link"
 					href="https://reactjs.org"
-					target="_blank"
+					  target="_blank"
 					 rel="noopener noreferrer">
 					Learn React
 				</a>
