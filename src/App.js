290fafc458ae4ea8cae2dc0dc8b7e22a15513de2
sbramd <trickster0179@gmail.com>
2020-03-05 16:59:36 +0100

    Plese work ffs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import logo from "./logo.svg" ;
-import "./App.css"
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
 	return (
@@ -10,16 +10,16 @@ function App() {
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
-			<a
+				<a
 					className="App-link"
 					href="https://reactjs.org"
-					  target="_blank"
-					 rel="noopener noreferrer">
+					target="_blank"
+					rel="noopener noreferrer">
 					Learn React
 				</a>
 			</header>
-		  </div>  
+		</div>
 	);
-	}
+}
 
 export default App;
